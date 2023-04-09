6e9b16059f2ae26141d2c0fb83c60f890d4e07a9
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-04-09 23:27:16 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,12 @@ import { Statistics } from './Statistics/Statistics';
 import { Wrapper } from './App.styled';
 import { useState } from 'react';
 
+const buttons = ['good', 'neutral', 'bad'];
+
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const buttons = ['good', 'neutral', 'bad'];
 
   // const handleIncrement = stateName => {
   //   this.setState(prevState => ({
